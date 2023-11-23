c2a7fb1557a3547fa2f3161031583b27fa8a2cd7
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-23 10:15:16 +0900

    Title: 블럭쌓는 명령2, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231123/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int n, k;
+	public static int[] a = new int[101];
+	public static int[] b = new int[101];
+	public static int[] blocks = new int[101];
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		for (int i = 1; i <= k; i++) {
+			st = new StringTokenizer(br.readLine());
+			a[i] = Integer.parseInt(st.nextToken());
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < k; i++) {
+			for (int j = a[i]; j <= b[i]; j++) {
+				blocks[i]++;
+			}
+		}
+
+		int max = 0;
+		for (int i = 1; i <= n; i++) {
+			if (blocks[i] > max) {
+				max = blocks[i];
+			}
+
+		}
+
+		System.out.println(max);
+
+	}
+
+}
