f6ebe3ff7d7d8bcc7e77d87a2d3f71505260c6da
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-28 09:56:45 +0900

    Title: 달에서 무게 구하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 231128/달에서 무게 구하기/weight-on-the-moon.java
+++ 231128/달에서 무게 구하기/weight-on-the-moon.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int w = 13;
         double r = 0.165;
-        System.out.printf("%d * %f = %f", w, r, w * r);
+        System.out.printf("%d * %.6f = %.6f", w, r, w * r);
     }
 }
