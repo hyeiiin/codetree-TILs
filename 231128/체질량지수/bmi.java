18170653e61de63add044831c6c0550c26356903
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-28 15:36:10 +0900

    Title: 체질량지수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/체질량지수/bmi.java
+++ 231128/체질량지수/bmi.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt(), w = sc.nextInt();
-        double bmi = w / ((double)h / 100 * (double)h / 100);
-        System.out.printf("%d\n", (int)bmi);
+        int bmi = w * 100 * 100 / (h * h);
+        System.out.printf("%d\n", bmi);
         if (bmi >= 25) System.out.println("Obesity");
     }
 }
