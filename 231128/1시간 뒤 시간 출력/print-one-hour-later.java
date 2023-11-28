940b50f773187c5a2356b18d862be50da543b7c4
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-28 14:04:49 +0900

    Title: 1시간 뒤 시간 출력, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231128/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":");
+        int h = sc.nextInt(), m = sc.nextInt();
+        System.out.printf("%d:%d", h + 1, m);
+    }
+}
