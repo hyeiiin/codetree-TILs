d6651e0ba7c36f7261e4c5b48495ce3175174a93
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-28 16:33:46 +0900

    Title: 삼항연산자, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/삼항연산자/ternary-operator.java
+++ 231128/삼항연산자/ternary-operator.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
-        if (score == 100) System.out.println("pass");
-        else System.out.println("failure");
+        System.out.println(score == 100 ? "pass" : "failure");
     }
 }
