c1de9ba5d2e49dfbce44f84b92e8bcd758a2309f
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-28 14:41:37 +0900

    Title: 입력받아 연산하기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/입력받아 연산하기/take-input-and-operate.java
+++ 231128/입력받아 연산하기/take-input-and-operate.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
-        System.out.println(a + 87);
-        System.out.println(b % 10);
+        a += 87;
+        b %= 10;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
