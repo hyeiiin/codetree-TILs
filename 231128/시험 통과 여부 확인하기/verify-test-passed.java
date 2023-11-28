9df8fd835d715c2619dcbceef1a7e9802ebe8003
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-28 15:54:48 +0900

    Title: 시험 통과 여부 확인하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/시험 통과 여부 확인하기/verify-test-passed.java
+++ 231128/시험 통과 여부 확인하기/verify-test-passed.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n > 80) System.out.println("pass");
+        if (n >= 80) System.out.println("pass");
         else System.out.printf("%d more score\n", 80 - n);
     }
 }
