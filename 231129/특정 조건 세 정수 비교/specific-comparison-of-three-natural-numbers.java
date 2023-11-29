e7ad5f281c53481f742f8855451a2007c4a9fdb7
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-29 10:03:35 +0900

    Title: 특정 조건 세 정수 비교, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

--- 231129/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 231129/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -3,10 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
-        int min = a;
-        if (min > b) min = b;
-        if (min > c) min = c;
-        if (a == min) System.out.print("1 ");
+        if (a <= b && a <= c) System.out.print("1 ");
         else System.out.print("0 ");
         if (a == b && b == c) System.out.print("1");
         else System.out.print("0");
