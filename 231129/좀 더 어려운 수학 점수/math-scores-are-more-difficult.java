92989fa9db30f8bca96cf0bab3e3092e07efc46c
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-29 10:58:52 +0900

    Title: 좀 더 어려운 수학 점수, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231129/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt(), aEng = sc.nextInt();
+        int bMath = sc.nextInt(), bEng = sc.nextInt();
+        if (aMath > bMath) System.out.print("A");
+        else if (aMath < bMath) System.out.print("B");
+        else if (aEng > bEng) System.out.print("A");
+        else System.out.print("B");
+    }
+}
