9ae071e2b3930ee5b6c29350b97b667453976b52
hyeiiin <126502807+hyeiiin@users.noreply.github.com>
2023-11-29 10:38:13 +0900

    Title: 장학금, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

--- 231129/장학금/scholarship.java
+++ 231129/장학금/scholarship.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int midScore = sc.nextInt(), finalScore = sc.nextInt();
-        if (midScore >= 90) {
-            if (finalScore >= 95) System.out.print(100000);
-            else if (finalScore >= 90) System.out.print(50000);
-            else System.out.print(0);
-        }
+        int m = sc.nextInt(), f = sc.nextInt();
+        if (m >= 90 && f >= 95) System.out.print(100000);
+        else if (m >= 90 && f >= 90) System.out.print(50000);
         else System.out.print(0);     
     }
 }
